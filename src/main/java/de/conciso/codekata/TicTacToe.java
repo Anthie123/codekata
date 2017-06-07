e76df4b4646c57e8d805b83ad04ce8155387ccca
Anthie123 <andrej.thiele@conciso.de>
2017-06-07 21:54:54 +0200

    inital commit

---
+++ src/main/java/de/conciso/codekata/TicTacToe.java
@@ -0,0 +1,13 @@
+package de.conciso.codekata;
+
+/**
+ * Hello world!
+ *
+ */
+public class TicTacToe
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/de/conciso/codekata/TicTacToeTest.java
@@ -0,0 +1,16 @@
+package de.conciso.codekata;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class TicTacToeTest
+{
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
